04b3f6732dd68e3eda99dae8030156fb4f7f6985
boryana.gm <boryana.gm@gmail.com>
2021-04-05 11:21:37 +0300

    Add ShipmentConfig

---
+++ DeliverIT/DeliverIT.Database/DataConfigurations/ShipmentConfig.cs
@@ -0,0 +1,27 @@
+﻿using DeliverIT.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Deliverit.Database.DataConfigurations
+{
+    public class ShipmentConfig : IEntityTypeConfiguration<Shipment>
+    {
+        public void Configure(EntityTypeBuilder<Shipment> builder)
+        {
+            builder.HasKey(s => s.Id);
+
+
+            // builder.Property(s => s.DepartureDate)
+            // builder.Property(s => s.ArrivalDate)
+            // builder.Property(s => s.Status)
+
+
+            builder.HasOne(s => s.Warehouse)
+                   .WithMany(w => w.Shipments)
+                   .HasForeignKey(s => s.WarehouseId);
+
+            // TODO: Should I describe the relation with Parcels?
+        }
+    }
+}
