308deb8e98b218d74f1bc19c9a1ad60daebcfb66
boryana.gm <boryana.gm@gmail.com>
2021-04-05 10:44:33 +0300

    Add CountryConfig class

---
+++ DeliverIT/DeliverIT.Database/DataConfigurations/CountryConfig.cs
@@ -0,0 +1,19 @@
+﻿using Deliverit.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deliverit.Database.DataConfigurations
+{
+    public class CountryConfig : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            // TODO: Should I describe the relation with Cities?
+        }
+    }
+}
