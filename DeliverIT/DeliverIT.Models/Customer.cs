4f1c191c50ae167bf669c6987c9842a93ddabb0e
boryana.gm <boryana.gm@gmail.com>
2021-04-05 14:19:51 +0300

    Remove abstract class User

--- DeliverIT/DeliverIT.Models/Customer.cs
+++ DeliverIT/DeliverIT.Models/Customer.cs
@@ -1,10 +1,18 @@
-﻿using DeliverIT.Models.Abstract;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace DeliverIT.Models
 {
-    public class Customer : User
+    public class Customer
     {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Guid AddressId { get; set; }
+        public Address Address { get; set; }
+
+        public ICollection<Parcel> Parcels { get; set; }
     }
 }
--- DeliverIT/DeliverIT.Models/Employee.cs
+++ DeliverIT/DeliverIT.Models/Employee.cs
@@ -1,11 +1,19 @@
-﻿using DeliverIT.Models.Abstract;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DeliverIT.Models
 {
-    public class Employee : User
+    public class Employee
     {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Guid AddressId { get; set; }
+        public Address Address { get; set; }
+
+        public ICollection<Parcel> Parcels { get; set; }
     }
 }
