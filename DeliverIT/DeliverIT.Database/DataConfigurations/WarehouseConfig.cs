0d9386e9f354685230c9863eb621108ede30946f
boryana.gm <boryana.gm@gmail.com>
2021-04-05 11:28:53 +0300

    Add WarehouseConfig

---
+++ DeliverIT/DeliverIT.Database/DataConfigurations/WarehouseConfig.cs
@@ -0,0 +1,25 @@
+﻿using DeliverIT.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Deliverit.Database.DataConfigurations
+{
+    public class WarehouseConfig : IEntityTypeConfiguration<Warehouse>
+    {
+        public void Configure(EntityTypeBuilder<Warehouse> builder)
+        {
+            builder.HasKey(w => w.Id);
+
+            // builder.Property(w => w.Category)
+
+            builder.HasOne(w => w.Address)
+                   .WithOne(a => a.Warehouse)
+                   .HasForeignKey<Address>(w => w.Warehouse);
+
+            // TODO: Should I describe the relation with Shipments?
+
+            // TODO: Should I describe the relation with Parcels?
+        }
+    }
+}
