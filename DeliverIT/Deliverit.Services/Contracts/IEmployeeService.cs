378316ef29a038439dfc3579543e0b89ed9f730c
boryana.gm <boryana.gm@gmail.com>
2021-04-09 18:02:13 +0300

    Create WarehouseService

---
+++ DeliverIT/Deliverit.Services/Contracts/IEmployeeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deliverit.Services.Contracts
+{
+    public interface IEmployeeService
+    {
+
+    }
+}
---
+++ DeliverIT/Deliverit.Services/Contracts/IWarehouseService.cs
@@ -0,0 +1,15 @@
+﻿using DeliverIT.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Deliverit.Services.Contracts
+{
+    public interface IWarehouseService
+    {
+        Warehouse Get(Guid id);
+        IEnumerable<Warehouse> GetAll();
+        Warehouse Create(Warehouse warehouse);
+        Warehouse Update(Guid id, Warehouse warehouse);
+        bool Delete(Guid id);
+    }
+}
