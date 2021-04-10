1050e1ea34bf1bb61e46c55f371523c42d12dc83
boryana.gm <boryana.gm@gmail.com>
2021-04-10 20:46:16 +0300

    Create CustomersController (must)

--- DeliverIT/Deliverit.Web/Controllers/WarehousesController.cs
+++ DeliverIT/Deliverit.Web/Controllers/WarehousesController.cs
@@ -33,7 +33,7 @@ namespace Deliverit.Web.Controllers
         {
             var warehouseToUpdate = this.warehouseService.Create(warehouse);
 
-            return this.Created("post", warehouse);
+            return this.Created("post", warehouseToUpdate);
         }
 
         [HttpPut("{id}")]
