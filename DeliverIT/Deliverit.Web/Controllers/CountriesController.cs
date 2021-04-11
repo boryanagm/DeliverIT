05fdc7a5960af0a32e0558e374e43286b8ee262e
Stefan Zhuntovski <stefan.zhuntovski.a26@learn.telerikacademy.com>
2021-04-12 01:56:49 +0300

    Added CountriesController

---
+++ DeliverIT/Deliverit.Web/Controllers/CountriesController.cs
@@ -0,0 +1,37 @@
+﻿using Deliverit.Services.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deliverit.Web.Controllers
+{
+    namespace Deliverit.Web.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class CountriesController : ControllerBase
+        {
+            private readonly ICountryService countryService;
+
+            public CountriesController(ICountryService countryService)
+            {
+                this.countryService = countryService;
+            }
+
+            [HttpGet("{id}")]
+            public IActionResult Get(Guid id)
+            {
+                return this.Ok(this.countryService.Get(id));
+            }
+
+            [HttpGet("")]
+            public IActionResult GetAll()
+            {
+                return this.Ok(this.countryService.GetAll());
+            }
+        }
+    }
+}
