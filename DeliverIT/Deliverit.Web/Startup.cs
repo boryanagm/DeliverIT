b5711e02cb9765ff38853bf7a3a3bbe45a687c2f
boryana.gm <boryana.gm@gmail.com>
2021-04-11 20:38:43 +0300

    Connect to Swagger

--- DeliverIT/Deliverit.Web/Startup.cs
+++ DeliverIT/Deliverit.Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Deliverit.Web
 {
@@ -24,6 +25,11 @@ namespace Deliverit.Web
             services.AddDbContext<DeliveritDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Configuration", Version = "v1" });
+            });
+
             services.AddScoped<ICityService, CityService>();
             services.AddScoped<ICustomerService, CustomerService>();
             services.AddScoped<IWarehouseService, WarehouseService>();
@@ -37,9 +43,14 @@ namespace Deliverit.Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseRouting();
 
-          //  app.UseAuthorization();
+            //  app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
