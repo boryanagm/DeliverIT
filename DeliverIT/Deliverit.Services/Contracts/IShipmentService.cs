2868107bcb235c05855b7f003d5e8a69d2c9eceb
Stefan Zhuntovski <stefan.zhuntovski.a26@learn.telerikacademy.com>
2021-04-12 16:12:17 +0300

    Added a Shipment Service.

---
+++ DeliverIT/Deliverit.Services/Contracts/IShipmentService.cs
@@ -0,0 +1,16 @@
+﻿using DeliverIT.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deliverit.Services.Contracts
+{
+    public interface IShipmentService
+    {
+        Shipment Get(Guid id);
+        IEnumerable<Shipment> GetAll();
+        Shipment Create(Shipment shipment);
+        Shipment Update(Guid id, Shipment shipment);
+        bool Delete(Guid id);
+    }
+}
--- DeliverIT/Deliverit.Services/ShipmentService.cs
+++ DeliverIT/Deliverit.Services/ShipmentService.cs
@@ -1,10 +1,64 @@
-﻿using System;
+﻿using Deliverit.Services.Contracts;
+using DeliverIT.Database;
+using DeliverIT.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Deliverit.Services
 {
-    class ShipmentService
+    public class ShipmentService : IShipmentService
     {
+        private readonly DeliveritDbContext context;
+
+        public ShipmentService(DeliveritDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Shipment Get(Guid id)
+        {
+            var shipment = this.context.Shipments
+                .FirstOrDefault(w => w.Id == id)
+                ?? throw new ArgumentNullException();
+
+            return shipment;
+        }
+
+        public IEnumerable<Shipment> GetAll()
+        {
+            var shipments = this.context.Shipments;
+
+            return shipments;
+        }
+
+        public Shipment Update(Guid id, Shipment shipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shipment Create(Shipment shipment)
+        {
+            this.context.Shipments.Add(shipment);
+            return shipment;
+        }
+
+        public bool Delete(Guid id)
+        {
+            var shipment = this.context.Shipments
+              .FirstOrDefault(w => w.Id == id);
+
+            if (shipment == null)
+            {
+                return false;
+            }
+            else
+            {
+                this.context.Shipments.Remove(shipment);
+                return true;
+            }
+        }
+        
     }
 }
