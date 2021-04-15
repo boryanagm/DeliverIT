84f3a321d12e2158dffd7368694145c6ca1c6ed6
boryana.gm <boryana.gm@gmail.com>
2021-04-15 18:46:03 +0300

    Create CityDTO

---
+++ DeliverIT/Deliverit.Services/Models/CityDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deliverit.Services.Models
+{
+    public class CityDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
