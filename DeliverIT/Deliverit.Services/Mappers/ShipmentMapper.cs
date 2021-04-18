9a1ddc9e62ac79d9a0099030305bf60aa5fa9d32
boryana.gm <boryana.gm@gmail.com>
2021-04-18 23:29:13 +0300

    Create ShipmentMapper

---
+++ DeliverIT/Deliverit.Services/Mappers/ShipmentMapper.cs
@@ -0,0 +1,19 @@
+﻿using Deliverit.Services.Models;
+using DeliverIT.Models;
+using System;
+using System.Linq.Expressions;
+
+namespace Deliverit.Services.Mappers
+{
+    public static class ShipmentMapper
+    {
+        public static Expression<Func<Shipment, ShipmentDTO>> DTOSelector = s =>
+           new ShipmentDTO
+           {
+               Id = s.Id,
+               Status = s.Status.Name,
+               DepartureDate = s.DepartureDate,
+               ArrivalDate = s.ArrivalDate
+           };
+    }
+}
