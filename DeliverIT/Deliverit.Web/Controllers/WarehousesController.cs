a603d40f8836ebb894f706c30219abdd08822f8e
Stefan Zhuntovski <stefan.zhuntovski.a26@learn.telerikacademy.com>
2021-04-19 23:38:09 +0300

    Merging stefan into developement.

--- DeliverIT/Deliverit.Web/Controllers/WarehousesController.cs
+++ DeliverIT/Deliverit.Web/Controllers/WarehousesController.cs
@@ -69,7 +69,7 @@ namespace Deliverit.Web.Controllers
             }
             catch (Exception)
             {
-                return Conflict();
+                return this.Conflict();
             }
         }
 
